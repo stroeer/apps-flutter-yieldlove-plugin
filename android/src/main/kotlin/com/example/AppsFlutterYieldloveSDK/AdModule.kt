2d37bb7ceb4274715514f84d274c794827029be7
Timo Bähr <timo.baehr@gmail.com>
2021-01-28 19:42:09 +0100

    Delete ad id prefix (but uncomment code). On real device ad load fails, but on Android emulator it works...

--- android/src/main/kotlin/com/example/AppsFlutterYieldloveSDK/AdModule.kt
+++ android/src/main/kotlin/com/example/AppsFlutterYieldloveSDK/AdModule.kt
@@ -2,8 +2,8 @@ package com.example.AppsFlutterYieldloveSDK
 
 class Ad(val id: String, val keyword: String? = null) {
     // TODO: Which prefix is correct?
-    val AD_UNIT_PREFIX = "/4444/m.app.droid_toi_sd"
+    //val AD_UNIT_PREFIX = "/4444/m.app.droid_toi_sd"
     //val AD_UNIT_PREFIX = "m.app.ios_toi_t-o_wetter_sd"
 
-    val adUnitId: String = "$AD_UNIT_PREFIX/$id"
+    val adUnitId: String = id// "$AD_UNIT_PREFIX/$id"
 }
