f12a4e0811da894ad932fbec749f8f649522398a
Timo Bähr <timo.baehr@gmail.com>
2021-02-15 14:55:26 +0100

    Use appId appDfpTest instead

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,7 +13,7 @@ import 'package:AppsFlutterYieldloveSDK/YieldloveWrapper.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await YieldloveWrapper.instance.initialize(
-      appId: "t-online_wetter_flutter",
+      appId: "appDfpTest",
       analyticsEnabled: false
   ).then((value) {
     print("app-widget: initialized = $value");
@@ -43,47 +43,54 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final adParams = AdCreationParams(
+        adId: 'banner',
+        optimalHeight: 100,
+        adKeyword: null,
+        adContentUrl: 'https://www.google.com',
+        useTestAds: false,
+        adIsRelease: false,
+        customTargeting: {"testKey": "testValue"}
+    );
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              RaisedButton(
-                onPressed: () {
-                  YieldloveWrapper.instance.showInterstitial(adUnitId: "/4444/m.app.dev.test/start_int");
-                },
-                child: Text("Show interstitial"),
-              ),
-              Text('Yieldlove native ad view:'),
-              YieldloveAdView(
-                  adParamsParcel: AdCreationParams(
-                    adId: 'start_b4',
-                    optimalHeight: 250,
-                    adKeyword: null,
-                    adContentUrl: 'https://www.google.com',
-                    useTestAds: false,
-                    adIsRelease: false,
-                    customTargeting: {"testKey": "testValue"}
-                  ),
-                  onPlatformViewCreated: (YieldloveAdController controller) {
-                    controller.listener = (YieldAdEvent event) {
-                      print("BannerAd event $event");
-                    };
-                    controller.showAd();
-                  }
-              ),
+        body: LayoutBuilder(builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+                constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                child: IntrinsicHeight(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      RaisedButton(
+                        onPressed: () {
+                          YieldloveWrapper.instance.showInterstitial(adUnitId: "interstitial");
+                        },
+                        child: Text("Show interstitial"),
+                      ),
+                      Text('Native ad with id "${adParams.adId}":'),
+                      YieldloveAdView(
+                          adParamsParcel: adParams,
+                          onPlatformViewCreated: (YieldloveAdController controller) {
+                            controller.listener = (YieldAdEvent event) {
+                              print("BannerAd event $event");
+                            };
+                            controller.showAd();
+                          }
+                      ),
 
-              Padding(
-                padding: const EdgeInsets.only(top: 400),
-                child: Text('bottom view'),
-              ),
-            ],
-          ),
+                      Expanded(child: Container()),
+                      Text('the bottom of the screen'),
+                    ],
+                  ),
+                )
+            )
+          );
+        },
         ),
       ),
     );
