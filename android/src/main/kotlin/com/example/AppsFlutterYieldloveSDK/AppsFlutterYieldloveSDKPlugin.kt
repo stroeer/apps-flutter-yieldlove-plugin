d66d01025d1b50737065ece8693a0f9abdd5df6d
Artem Yuzhakov <artem.yuzhakov@stroeer.de>
2021-10-12 12:40:22 +0200

    finish migration to Yieldlove Sourcepoint(almost)

--- android/src/main/kotlin/com/example/AppsFlutterYieldloveSDK/AppsFlutterYieldloveSDKPlugin.kt
+++ android/src/main/kotlin/com/example/AppsFlutterYieldloveSDK/AppsFlutterYieldloveSDKPlugin.kt
@@ -1,6 +1,14 @@
 package com.example.AppsFlutterYieldloveSDK
 
+import android.R
+import android.app.Activity
 import android.util.Log
+import android.view.View
+import com.sourcepoint.gdpr_cmplibrary.GDPRUserConsent
+import com.yieldlove.adIntegration.YieldloveConsent
+import android.view.ViewGroup
+import com.sourcepoint.gdpr_cmplibrary.ConsentLibException
+
 import androidx.annotation.NonNull
 import com.example.AppsFlutterYieldloveSDK.ad_view.NativeAdViewFactory
 import com.yieldlove.adIntegration.Yieldlove
@@ -11,8 +19,7 @@ import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
-import java.lang.Exception
+import com.yieldlove.adIntegration.YieldloveConsentListener
 
 /** AppsFlutterYieldloveSDKPlugin */
 class AppsFlutterYieldloveSDKPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
@@ -22,16 +29,38 @@ class AppsFlutterYieldloveSDKPlugin: FlutterPlugin, MethodCallHandler, ActivityA
   }
 
   private lateinit var channel : MethodChannel
+  private var activity: Activity? = null
 
-  override fun onMethodCall(call: MethodCall, result: Result) {
+  override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
     when (call.method) {
       "initialize" -> callInitialize(call, result)
       "loadInterstitialAd" -> callInterstitialLoad(call, result)
+      "showConsent" -> showConsent(call, result)
+      "showPrivacyManager" -> showPrivacyManager(call, result)
       else -> result.notImplemented()
     }
   }
 
-  private fun callInitialize(call: MethodCall, result: Result) {
+  private fun showConsent(call: MethodCall, result: MethodChannel.Result) {
+    if (activity != null) {
+      val authId: String? = call.argument("authId")
+      val consent = if (authId != null) {
+        YieldloveConsent(activity, ConsentListener(activity?.findViewById(R.id.content), channel), authId)
+      } else {
+        YieldloveConsent(activity, ConsentListener(activity?.findViewById(R.id.content), channel))
+      }
+      consent.collect()
+    }
+  }
+
+  private fun showPrivacyManager(call: MethodCall, result: MethodChannel.Result) {
+    if (activity != null) {
+      val consent = YieldloveConsent(activity, R.id.content)
+      consent.showPrivacyManager()
+    }
+  }
+
+  private fun callInitialize(call: MethodCall, result: MethodChannel.Result) {
     val appId: String? = call.argument("appId")
     if (appId == null || appId.isEmpty()) {
       result.error("no_app_id", "a null or empty AdMob appId was provided", null)
@@ -46,7 +75,7 @@ class AppsFlutterYieldloveSDKPlugin: FlutterPlugin, MethodCallHandler, ActivityA
     }
   }
 
-  private fun callInterstitialLoad(call: MethodCall, result: Result) {
+  private fun callInterstitialLoad(call: MethodCall, result: MethodChannel.Result) {
     InterstitialHolder.delegateMethodCall(call, result)
   }
 
@@ -57,8 +86,10 @@ class AppsFlutterYieldloveSDKPlugin: FlutterPlugin, MethodCallHandler, ActivityA
     binding.getFlutterEngine()
             .getPlatformViewsController()
             .getRegistry()
-            .registerViewFactory("de.stroeer.plugins/yieldlove_ad_view",
-                    NativeAdViewFactory(messenger,  /*containerView=*/null))
+            .registerViewFactory(
+              "de.stroeer.plugins/yieldlove_ad_view",
+              NativeAdViewFactory(messenger,  /*containerView=*/null)
+            )
     
     channel = MethodChannel(binding.binaryMessenger, "AppsFlutterYieldloveSDK")
     channel.setMethodCallHandler(this)
@@ -72,6 +103,7 @@ class AppsFlutterYieldloveSDKPlugin: FlutterPlugin, MethodCallHandler, ActivityA
 
   override fun onAttachedToActivity(binding: ActivityPluginBinding) {
     Log.v(TAG, "onAttachedToActivity")
+    activity = binding.activity
     binding.activity?.let {
       AdPlatformView.activity = it
       InterstitialHolder.activity = it
@@ -98,6 +130,7 @@ class AppsFlutterYieldloveSDKPlugin: FlutterPlugin, MethodCallHandler, ActivityA
   override fun onDetachedFromActivityForConfigChanges() {
     Log.v(TAG, "onDetachedFromActivityForConfigChanges")
 
+    activity = null
     // The Activity your plugin was associated with has been
     // destroyed due to config changes. It will be right back
     // but your plugin must clean up any references to that
@@ -121,5 +154,51 @@ class AppsFlutterYieldloveSDKPlugin: FlutterPlugin, MethodCallHandler, ActivityA
     // You must clean up all resources and references. Your
     // plugin may, or may not ever be associated with an Activity
     // again.
+  }
+}
+
+
+open class ConsentListener(
+  private val parent: ViewGroup?,
+  private val channel: MethodChannel?
+) : YieldloveConsentListener {
+
+  override fun OnError(error: ConsentLibException?) {
+    channel?.invokeMethod("onError", error?.consentLibErrorMessage)
+  }
+
+  override fun OnConsentUIReady(consentView: View) {
+    consentView.layoutParams = ViewGroup.LayoutParams(
+      ViewGroup.LayoutParams.MATCH_PARENT,
+      ViewGroup.LayoutParams.MATCH_PARENT
+    )
+    consentView.bringToFront()
+    consentView.requestLayout()
+    parent?.addView(consentView)
+
+    channel?.invokeMethod("onConsentUIReady", null)
+  }
+
+  override fun OnConsentUIFinished(consentView: View?) {
+    parent?.removeView(consentView)
+    channel?.invokeMethod("onConsentUIFinished", null)
+  }
+
+  override fun OnConsentReady(consent: GDPRUserConsent) {
+    //Config.USER_GAVE_CONSENT = consent.acceptedCategories.isNotEmpty()
+    //val oldConsentString = Config.USER_CONSENT_STRING
+    //if (oldConsentString == null || !Config.USER_CONSENT_STRING.equals(consent.consentString)) {
+    //  Config.USER_CONSENT_STRING = consent.consentString
+    //  TomoTracker.trackEvent(ConsentAcceptEvent())
+    //  onAcceptClicked()
+    //}
+    val map: HashMap<String, Any> = hashMapOf<String, Any>(
+      "consentString" to consent.consentString,
+      "acceptedVendors" to consent.acceptedVendors,
+      "acceptedCategories" to consent.acceptedCategories,
+      "legIntCategories" to consent.legIntCategories,
+      "specialFeatures" to consent.specialFeatures
+    )
+    channel?.invokeMethod("onConsentReady", map)
   }
 }
